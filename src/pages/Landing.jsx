36d4de42ccf3ab70790d41fcfc3545256cb09f32
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-02-28 01:07:28 +0530

    add background changes to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ImageSlider, Categories } from '../components';
 import {
   QuickLinks,
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { HiOutlinePlusSm, HiOutlineMinusSm } from 'react-icons/hi';
 import { useParams } from 'react-router-dom';
